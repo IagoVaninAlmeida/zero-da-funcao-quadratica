51aadce03d852ab41f69fa6c448ec00eaf7ad20a
IagoVaninAlmeida <159267283+IagoVaninAlmeida@users.noreply.github.com>
2024-09-16 09:24:31 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,34 +1,18 @@
-let a = parseFloat(prompt("p"));
-let b = parseFloat(prompt("i"));
-let c = parseFloat(prompt("n"));
-let delta = (b*b)*(-4*a*c);
-delta = Math.sqrt(delta);
-let x_1 = (-b + delta)/(2*a);
-let x_2 = (-b - delta)/(2*a);
-
-
-
-
-
-
-function calcula() {
-    const num1 = parseFloat(document.getElementById('zeroum').value);
-    const num2 = parseFloat(document.getElementById('zerodois').value);
-    const num3 = parseFloat(document.getElementById('zerotres').value);
-    let resultado = 0;
-    let oper = document.querySelector('input[name="operacao"]:checked').value;
-
-    if(oper == 'soma'){
-        resultado = num1 + num2;
-    }else if(oper == 'subtracao'){
-        resultado = num1 - num2;
-    }else if(oper == 'multiplicacao'){
-        resultado = num1 * num2;
-    }else if(oper == 'divisao'){
-        resultado = num1/num2;
-    }else if(oper == 'porcentagem'){
-        resultado = num1*num2/100;
+function calcula(){
+    let a = parseFloat(document.getElementById('a').value);
+    let b = parseFloat(document.getElementById('b').value);
+    let c = parseFloat(document.getElementById('c').value);
+    let delta = (b*b)-(-4*a*c);
+    let rdelta = Math.sqrt(delta);
+     
+    if (delta == 0){
+        alert('sem resolução quando delta é igual a 0');
+    }else if (delta < 0){
+        let x1 = (-b)/(2*a);
+        document.getElementById('resultado').innerText = 'Resultado: ' + x1;
+    }else{
+        let x1 = (-b+rdelta)/(2*a);
+        let x2 = (-b-rdelta)/(2*a);
+        document.getElementById('resultado').innerText = 'Resultado: ' + x1 + ' e ' + x2;
     }
-
-    document.getElementById('resultado').innerText = resultado;
 }
